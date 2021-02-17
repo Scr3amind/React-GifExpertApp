9a86aa04f9d44470b4a9dfb34943eb0c4a134311
scr3amind <screamindarko@gmail.com>
2021-02-17 12:16:33 -0600

    App terminada

---
+++ src/GifExpertApp.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+    
+    const [categories, setCategories] = useState(['MR ROBOT']);
+    
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={setCategories} />   
+            <ol>
+                {
+                    categories.map(category => {
+                        return <GifGrid 
+                                    key={category}
+                                    category={category}
+                                />
+                    })
+                }
+            </ol>
+        </>
+    );
+}
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,17 @@
+import { GifGridItem } from "../components/GifGridItem";
+
+export const getGifs = async (category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=QLdm3V22gEBwWYtk6X6T2Lq92hgOqJ3l`;
+    const resp = await fetch(url);
+    const {data} = await resp.json();
+
+    const gifs = data.map( img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    });
+
+    return gifs;
+}
